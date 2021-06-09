e35cf84d44451e9be45d995e03a351f6236e5809
rattanaksar <81554110+rattanaksar@users.noreply.github.com>
2021-06-09 11:22:16 +0200

    Exo 7 PHP

---
+++ Exo 7.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exo 7 : Les variables PHP</title>
+</head>
+<body>
+    <h1>Exo 7 Partie : Les variables PHP</h1>
+    <?php
+        $lastName= "Sar";
+        $firstName= "Rattanak";
+        $age = 38;
+        echo "Je m'appelle " .$lastName. " " .$firstName. " et j'ai " .$age. " ans <br>";
+    ?>
+</body>
+</html>
